6b1353ee86d124837f4c625d086990f3f90951be
Vitor Furini <vitorfurini@hotmail.com>
2022-03-29 17:10:45 -0300

    First commit

---
+++ src/main/java/com/softdesign/vitorfurini/VitorfuriniApplication.java
@@ -0,0 +1,13 @@
+package com.softdesign.vitorfurini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitorfuriniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitorfuriniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softdesign/vitorfurini/VitorfuriniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softdesign.vitorfurini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitorfuriniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
